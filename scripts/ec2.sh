2a9ab34a88bdd38ba7b378805d463c7b451a1c61
Matthew Wen <mattwen2018@gmail.com>
2021-04-12 02:52:17 -0400

    added script for using amazon ec2 machine (#25)

---
+++ scripts/ec2.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")" || exit
+cd ../
+git fetch origin
+git reset --hard origin/main
+python3 setup.py install
+cp service/queueup-bot.service /lib/systemd/system 
+systemctl daemon-reload
+systemctl restart queueup-bot.service
+loginctl enable-linger $USER
+exit
