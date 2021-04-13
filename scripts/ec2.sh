f602709aab4769fe5f292619150451367771893c
Matthew Wen <mattwen2018@gmail.com>
2021-04-12 23:46:56 -0400

    Deployment on Amazon EC2 (#27)
    * update workflow for ec2 instance ci/cd
    
    * change python version to 3.7 instead of 3.8
    
    * updated requirements.txt
    
    * uninstall then reinstall on CI / CD

--- scripts/ec2.sh
+++ scripts/ec2.sh
@@ -3,6 +3,7 @@ cd "$(dirname "$0")" || exit
 cd ../
 git fetch origin
 git reset --hard origin/main
+pip3 uninstall queue_bot -y
 python3 setup.py install
 cp service/queueup-bot.service /lib/systemd/system 
 systemctl daemon-reload
