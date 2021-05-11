ce99e56bca4e84cfb0917bbc40eb34d2830836e9
Matthew Wen <mattwen2018@gmail.com>
2021-05-11 04:33:46 -0500

    Make it print if runs into 403, instead of end process

--- bot264/discord_wrapper.py
+++ bot264/discord_wrapper.py
@@ -23,7 +23,10 @@ class Permissions:
         for _, v in self.rooms.items():
             target_channel: discord.TextChannel = client.get_channel(v)
             if target_channel is not None:
-                await target_channel.set_permissions(student, overwrite=None)
+                try:
+                    await target_channel.set_permissions(student, overwrite=None)
+                except discord.Forbidden as e:
+                    print(e)
 
     async def set_access_to_text_channel(self, student, ta_voice_channel, state):
         client: discord.Client = DiscordWrapper.client
@@ -31,16 +34,20 @@ class Permissions:
         if ta_channel_id in self.rooms:
             target_channel: discord.TextChannel = client.get_channel(self.rooms[ta_channel_id])
             await target_channel.purge()
-            if state:
-                await target_channel.set_permissions(student, read_messages=state, send_messages=state)
-                message_to_student = """
-                You can have a 1 on 1 conversation here with your TA. 
-                Hopefully, this session is helpful.
-                """
-                discord_message = create_simple_message("Hey There {}".format(student.display_name), message_to_student)
-                await target_channel.send(embed=discord_message)
-            else:
-                await self.remove_permissions_from_all_rooms(student)
+            try:
+                if state:
+                    await target_channel.set_permissions(student, read_messages=state, send_messages=state)
+                    message_to_student = """
+                    You can have a 1 on 1 conversation here with your TA. 
+                    Hopefully, this session is helpful.
+                    """
+                    discord_message = create_simple_message("Hey There {}".format(student.display_name),
+                                                            message_to_student)
+                    await target_channel.send(embed=discord_message)
+                else:
+                    await self.remove_permissions_from_all_rooms(student)
+            except discord.Forbidden as e:
+                print(e)
 
 
 def get_db_connection(file_location):
