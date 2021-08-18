16497d89c27754ded83227bfd76d7144b73f1879
Matthew Wen <mattwen2018@gmail.com>
2021-08-17 23:46:30 -0500

    Fix Issue with Database Not Being Fully Setup

--- scripts/template_add_server.py
+++ scripts/template_add_server.py
@@ -1,66 +1,31 @@
 #!/usr/bin/python3
-import os
-os.environ['PRODUCTION'] = "1"
-os.environ['DATABASE'] = '../queueup.db'
-os.environ['DATABASE_DIRECTORY'] = '../test_queue_directory'
-from bot264.discord_wrapper import create_server_db, Db, get_db_connection, create_db
+from bot264.discord_wrapper import Db
+from bot264.mwenclubhouse import FbDb
 
+Db.database_file_location = '../queueup.db'
+Db.database_folder_location = '../test_queue_directory'
 server_id = 0
 bot_channel_id = 0
 waiting_room = 0
 uta_role_id = None
 gta_role_id = None
-professor_role_id = 0
+professor_role_id = None
 queue_channel_id = 0
 history_channel_id = 0
-
 # Rooms is {voice channel id: text channel id}
-rooms = {None: None}
+rooms = {0: 0}
 
-Db.database_file_location = os.getenv('DATABASE')
-Db.database_folder_location = os.getenv('DATABASE_DIRECTORY')
-create_db()
-connection = get_db_connection(Db.database_file_location)
-cursor = connection.cursor()
-command = f"""
-INSERT OR REPLACE INTO servers (server_id, bot_channel_id, waiting_room_id) VALUES({server_id}, {bot_channel_id}, {waiting_room}); 
-"""
-cursor.execute(command)
+ta_roles = dict()
+for i in [uta_role_id, gta_role_id, professor_role_id]:
+    if i is not None:
+        ta_roles[i] = ''
 
-if uta_role_id is not None:
-    command = f"""
-    INSERT OR REPLACE INTO teaching_roles (teaching_role_id, assigned_level, server_id) VALUES({uta_role_id}, 0, {server_id}); 
-    """
-    cursor.execute(command)
-
-if gta_role_id is not None:
-    command = f"""
-    INSERT OR REPLACE INTO teaching_roles (teaching_role_id, assigned_level, server_id) VALUES({gta_role_id}, 0, {server_id}); 
-    """
-    cursor.execute(command)
-
-if professor_role_id is not None:
-    command = f"""
-    INSERT OR REPLACE INTO teaching_roles (teaching_role_id, assigned_level, server_id) VALUES({professor_role_id}, 0, {server_id}); 
-    """
-    cursor.execute(command)
-
-command = f"""
-INSERT OR REPLACE INTO queues (queue_channel_id, history_channel_id, server_id) VALUES({queue_channel_id}, {history_channel_id}, {server_id}); 
-"""
-cursor.execute(command)
-cursor.close()
-connection.commit()
-connection.close()
-
-connection = create_server_db(server_id, force_create=True, return_connection=True)
-cursor = connection.cursor()
-for k, v in rooms.items():
-    if k is not None:
-        command = f"""
-        INSERT OR REPLACE INTO rooms (room_voice_channel_id, room_text_channel_id) VALUES ({k}, {v});
-        """
-        cursor.execute(command)
-cursor.close()
-connection.commit()
-connection.close()
+FbDb.on_set(server_id, {
+    'queues': {
+        queue_channel_id: history_channel_id
+    },
+    'ta_roles': ta_roles,
+    'bot': bot_channel_id,
+    'waiting': waiting_room,
+    'rooms': {k: v for k, v in rooms.items() if not (None in [k, v])}
+})
